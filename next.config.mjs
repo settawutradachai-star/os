c6c77187f453fa8497c82cdc5722bdfb6f0bca99
settawutradachai <settawut.radachai@e-tech.ac.th>
2026-05-28 17:38:22 +0700

    fix: remove middleware, force nodejs runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['pg'],
+  },
 };
 
 export default nextConfig;
